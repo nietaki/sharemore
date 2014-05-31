8fb401109a7877c136ab2db6f069212d833ab594
Jacek Królikowski <nietaki@gmail.com>
2014-05-31 16:22:00 +0200

    some rudimentary css

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nietaki on 31.05.14.
+ */
+//dragover dragleavae
+// http://css-tricks.com/snippets/javascript/test-if-dragenterdragover-event-contains-files/
+//http://help.dottoro.com/external/examples/ljrkqflw/ondragover_1.htm
