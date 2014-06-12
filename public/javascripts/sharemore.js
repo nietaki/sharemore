49e35a6b3027decd69f4fb4d4ff1c28c5c69b336
Jacek Królikowski <nietaki@gmail.com>
2014-06-12 15:49:20 +0200

    angular seems to be working

---
+++ public/javascripts/sharemore.js
@@ -0,0 +1,19 @@
+/**
+ * Created by nietaki on 12.06.14.
+ */
+(function(){
+
+  console.log('sharemore.js')
+  var app = angular.module('sharemore', [])
+
+
+  app.controller('UploadController', function(){
+    console.log('uploader')
+    var upl = this
+    this.uploadStarted = false
+
+
+  });
+})();
+
+
